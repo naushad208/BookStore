17722138a0d24a54ad501b6536321a6f14c0a96b
naushad <naush@LAPTOP-T7GRQCV9>
2021-03-02 13:41:26 +0300

    add mvc

---
+++ MHamzaBookStore/MHamzaBookStore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHamzaBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string index()
+        {
+            return "Hello from controller";
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
--- MHamzaBookStore/MHamzaBookStore/Startup.cs
+++ MHamzaBookStore/MHamzaBookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace MHamzaBookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,17 +25,18 @@ namespace MHamzaBookStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
+            }            
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.MapGet("/", async context =>
+                //{                    
+                //    await context.Response.WriteAsync(env.EnvironmentName);
+                //});
+            });            
         }
     }
 }
